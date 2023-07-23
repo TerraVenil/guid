5043ce7742f8b43cc455a50e5730c5b6815552fd
Marco Klein <marco.klein@avsn.de>
2023-07-23 20:05:51 +0200

    feat: :sparkles: Implement lower case generation command

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,24 @@ import * as vscode from 'vscode';
 import * as uuid from 'node-uuid';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.guid', () => {
-
+    const generateGuid = (lowercase) => () => {
         let textEditor = vscode.window.activeTextEditor;
 
         vscode.window.activeTextEditor.edit(builder => {
             textEditor.selections.forEach(sel => {
                 let result = uuid.v4();
-                builder.replace(sel, result.toUpperCase());
+                if (!lowercase) {
+                    result = result.toUpperCase();
+                }
+                builder.replace(sel, result);
             })
         });
-	});
-
-	context.subscriptions.push(disposable);
+	}
+	
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.guid', generateGuid()));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.guid_lowercase', generateGuid(true)));
 }
 
 export function deactivate() {
